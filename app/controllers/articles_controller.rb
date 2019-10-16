290de92956d766647af0d240d97d2cfebce3b867
Kaleb Haslam <kalebmarc@hotmail.com>
2019-10-16 07:42:58 +0000

    add show and edit button

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,6 +11,10 @@ class ArticlesController < ApplicationController
   def new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
     # we need to explicitly permit certain values to be sent to the model
     # as a security measure
@@ -20,6 +24,16 @@ class ArticlesController < ApplicationController
       redirect_to @article
     else
       render 'new'
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
     end
   end
 
